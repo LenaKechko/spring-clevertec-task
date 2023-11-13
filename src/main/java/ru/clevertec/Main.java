f87ece05d1553c505aa836bd336e488d0d161f6e
Elena Kechko <ekechko@gmail.com>
2023-11-13 19:29:18 +0300

    Create entity Animal.java and file with sql-request for zoodb

--- src/main/java/ru/clevertec/Main.java
+++ src/main/java/ru/clevertec/Main.java
@@ -1,7 +1,12 @@
 package ru.clevertec;
 
+import ru.clevertec.entity.Animal;
+
+import java.util.UUID;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Animal animal = new Animal(UUID.randomUUID(), "name", "type", "class", 1000.0, 1.5, 40.0);
+        System.out.println(animal.toString());
     }
 }
