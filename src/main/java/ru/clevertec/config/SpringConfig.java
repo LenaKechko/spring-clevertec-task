4d24d2fb1ab9c7664919ce64662c12fc641da8f6
Elena Kechko <ekechko@gmail.com>
2024-01-04 19:39:08 +0300

    Making cache

--- src/main/java/ru/clevertec/config/SpringConfig.java
+++ src/main/java/ru/clevertec/config/SpringConfig.java
@@ -7,17 +7,21 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import ru.clevertec.entity.Animal;
+import ru.clevertec.proxy.cache.IBaseCache;
+import ru.clevertec.proxy.cache.imp.LFUCache;
+import ru.clevertec.proxy.cache.imp.LRUCache;
 
 import javax.sql.DataSource;
-import java.util.Arrays;
 import java.util.Objects;
-import java.util.ResourceBundle;
+import java.util.UUID;
 
 @Configuration
 @ComponentScan(basePackages = "ru.clevertec")
+@EnableAspectJAutoProxy
 @PropertySource("classpath:application.yml")
 @Slf4j
 public class SpringConfig {
@@ -35,5 +39,17 @@ public class SpringConfig {
         dataSource.setPortNumbers(new int[]{Integer.parseInt(Objects.requireNonNull(env.getProperty("db.port")))});
         dataSource.setDatabaseName(env.getProperty("db.databaseName"));
         return dataSource;
+    }
+
+    @Bean
+    public IBaseCache<UUID, Animal> cache() {
+        String algorithmCache = env.getProperty("cache.algorithm");
+        Integer capacity = Integer.valueOf(Objects.requireNonNull(env.getProperty("cache.size")));
+        System.out.println(capacity);
+        return switch (Objects.requireNonNull(algorithmCache)) {
+            case "LRU" -> new LRUCache<>(capacity);
+            case "LFU" -> new LFUCache<>(capacity);
+            default -> throw new IllegalStateException("Unexpected value: " + Objects.requireNonNull(algorithmCache));
+        };
     }
 }
